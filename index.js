e6697676063b4312f6114eaddb11a0ad7ad73f52
Imee Cuison <imeetwelve@gmail.com>
2015-01-20 13:23:03 -0500

    Matt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
   "use strict";
   /// return ..
 }*/
-
+/*
 function sumTwo () {
 var numbers = [];
 var inputnumber1=prompt("Input a number you want to add")
@@ -22,4 +22,44 @@ for(var i=0; i < arrayLength; i++){
 
 console.log("SUM =" + (sum));
 
-}
+}*/
+
+/*getAverageTime*/
+
+/*function averageTime () {*/
+var person =("matt", "mark");
+
+var mattArray = [
+   240,
+   210.4,
+   235.1,
+   208.9,
+   197.5,
+];
+
+
+/*
+var mark = {
+  finishingTime1: 120,
+  finishingTime2: 110.4,
+  finishingTime3: 135.1,
+  finishingTime4: 108.9,
+  finishingTime5: 97.5
+};*/
+
+/*var getAverageTime = function(person) {*/
+
+var arrayLength = mattArray.length;
+var sum=0;
+var i;
+
+/*var combo = {*/
+for(var i=0; i < arrayLength; i++){
+    sum+= parseFloat(mattArray[i]);
+  }
+    /*var getAverageTime = combo/arrayLength;*/
+
+var getAverageTime = sum/arrayLength;
+
+
+/*}*/
